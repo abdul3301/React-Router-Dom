8b7eb6c3863887fa0f0dc9dc76a877bc15486615
abdul3301 <abdulrahimsilvery@gmail.com>
2023-05-30 17:50:28 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./Nav";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="*" element={<Dunno />} />
+      </Routes>
     </div>
   );
+}
+
+function Home() {
+  return <main>This is Home</main>;
+}
+function Users() {
+  return (
+    <main>
+      This is Users
+      {/* <Outlet /> */}
+      <Routes>
+        <Route path=":id" element={<User />} />
+        <Route path="me" element={<UserMe />} />
+      </Routes>
+    </main>
+  );
+}
+function User() {
+  return <main>This is User x</main>;
+}
+function UserMe() {
+  return <main>This is All about me</main>;
+}
+function Dunno() {
+  return <main>Donno this route</main>;
 }
 
 export default App;
